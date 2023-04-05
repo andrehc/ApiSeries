8d16105ff59aed8965db6496950aeee309c366da
Andre Costa <andre.hcosta@hotmail.com>
2023-04-04 22:09:01 -0300

    fix | import class to api

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -10,6 +10,6 @@ class SerieController extends Controller
 {
     public function show() : array
     {
-        return [ 'id' => 1, 'nome_serie' => 'Mad Man', 'created_at' => Carbon::now()->format('d-m-Y'), 'updated_at', null ];
+        return [ 'id' => 1, 'nome_serie' => 'Mad Man', 'created_at' => Carbon::now()->format('d-m-Y'), 'updated_at' => null ];
     }
 }
