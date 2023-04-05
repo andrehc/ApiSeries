3e5ade01492bf5ea920bf1be0464c809b3c565b0
Andre Costa <andre.hcosta@hotmail.com>
2023-04-04 22:03:31 -0300

    add getSeries method and routes.

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\Serie;
+use Carbon\Carbon;
 
 class SerieController extends Controller
 {
-    //
+    public function show() : array
+    {
+        return [ 'id' => 1, 'nome_serie' => 'Mad Man', 'created_at' => Carbon::now()->format('d-m-Y'), 'updated_at', null ];
+    }
 }
